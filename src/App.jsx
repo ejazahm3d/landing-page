d95354ed2a7e7afc1c00faaf81bb27c36674a179
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-22 12:47:19 +0500

    Completed hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react"
 import Layout from "./components/layout"
 import Hero from "./components/blocks/Hero"
-import { Button } from "@chakra-ui/core"
 
 function App() {
     return (
         <Layout>
             <Hero />
-            <Button variant="outline" variantColor="pink" size="md">
-                Hello
-            </Button>
         </Layout>
     )
 }
